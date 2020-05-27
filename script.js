4576ee7a51bad165209bb8b233782ebd16604dfa
Haden Wallin <haden.wallin@gmail.com>
2020-05-26 18:57:32 -0700

    add game functionality

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function computerPlay(){
-    return Math.random()*4
+    const options = ['rock', 'paper', 'scissors'];
+    const choice = options[Math.floor(Math.random()*3)]
+    console.log(choice)
+
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection){
+
+
+    if (playerSelection === computerSelection){
+        console.log('The game is  tie!')
+    }
+    else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        console.log(`You win!${playerSelection} beats ${computerSelection}`)}
+        
+    else{
+        console.log("you lose")
+    }
     
 
 }
+let playerSelection = prompt("enter rock paper or scissors").toLowerCase()
+playRound(playerSelection, computerPlay())
